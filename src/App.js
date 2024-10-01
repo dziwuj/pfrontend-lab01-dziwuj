12867cad983327850c8236282fe141a11f753a63
dziwuj <sobeckizbyszko@gmail.com>
2024-10-01 16:52:07 +0200

    Refactor App.js and CFooter.js components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg'
 import './App.css'
-import CFooter from './components/CFooter'
+import { CFooter } from './components/CFooter'
 
 function App() {
     return (
--- src/components/CFooter.js
+++ src/components/CFooter.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const CFooter = () => {
+export const CFooter = () => {
     let date = new Date()
 
     return (
         <div>
-            <h5>Programowanie JavaScript przykładowy serwis React. Dzisiaj mamy: {date.toString()}</h5>
+            <h5>
+                Prog. JS, przykładowy serwis React. Dzisiaj mamy: {'  '}
+                {date.toString()}
+            </h5>
         </div>
     )
 }
-
-export default CFooter
