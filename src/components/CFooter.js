38e23354fab847825cb32fe847114dee399036d5
dziwuj <sobeckizbyszko@gmail.com>
2024-10-01 16:50:35 +0200

    Refactor App.js and CFooter.js components

--- src/components/CFooter.js
+++ src/components/CFooter.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const CFooter = () => {
-    return <h5>Programowanie JavaScript przykładowy serwis React.</h5>
+    let date = new Date()
+
+    return (
+        <div>
+            <h5>Programowanie JavaScript przykładowy serwis React. Dzisiaj mamy: {date.toString()}</h5>
+        </div>
+    )
 }
 
 export default CFooter
